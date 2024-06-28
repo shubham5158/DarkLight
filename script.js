7ffe2e4b2fc1ba9157287294c470bd39d4c85069
shubham <shubhamgogawale1@gmail.com>
2024-06-28 17:07:32 +0530

    changes

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 let buttons = document.querySelectorAll(".button");
 let body = document.querySelector("body");
 
-buttons.forEach(function (button) {
-  button.addEventListener("mouseover", function (e) {
-    if (e.target.id === "grey") {
+buttons.forEach((button) => {
+  button.addEventListener("mouseover", (event) => {
+    if (event.target.id === "grey") {
       body.style.backgroundColor = "grey";
     }
-
-    if (e.target.id === "white") {
+    if (event.target.id === "white") {
       body.style.backgroundColor = "white";
     }
-
-    if (e.target.id === "blue") {
+    if (event.target.id === "blue") {
       body.style.backgroundColor = "blue";
     }
-
-    if (e.target.id === "yellow") {
+    if (event.target.id === "yellow") {
       body.style.backgroundColor = "yellow";
     }
-
-    if (e.target.id === "violet") {
+    if (event.target.id === "violet") {
       body.style.backgroundColor = "violet";
+    }
+    if (event.target.id === "black") {
+      body.style.backgroundColor = "black";
     }
   });
 });
